f042389bbe32104fc3d2535173d4fc98a44c2830
Emanuele-Guerra <emaguerra1993@hotmail.it>
2023-11-15 17:08:07 +0100

    push1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue;
+
+
+    data: function () {
+        return {
+            message: 'Benvenuto, ',
+            imgSrc: '33204321.png'
+        }
+    }
+    
+
+
+createApp.mount('#app')
